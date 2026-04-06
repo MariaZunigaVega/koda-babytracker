de48480098196308a86fa9eb8a7e19fb1029675d
Jacinta Thomas <jacintathomas@my.unt.edu>
2026-04-05 23:43:35 -0500

    login page with UI to match registration

--- koda-client/src/App.jsx
+++ koda-client/src/App.jsx
@@ -4,6 +4,7 @@ import ParentDashboard from './pages/ParentDashboard';
 import AvatarSelection from './pages/avatarSelection';
 import Welcome from './pages/welcome';
 import Registering from './pages/registering';
+import Login from './pages/Login';
 import ChildRegistration from './pages/childRegistration';
 import Activities from './pages/Activities';
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/registering" element={<Registering />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/avatarSelection" element={<AvatarSelection />} />
         <Route path="/childRegistration" element={<ChildRegistration />} />
         <Route path="/ParentDashboard" element={<ParentDashboard />} />
