53773cf6907beae665345e8c65b6ca582e92d943
Jacinta Thomas <jacintathomas@my.unt.edu>
2026-08-27 12:12:22 -0500

    Revert "fix: resolved duplicate declarations"
    This reverts commit 724227ddbfb4e3ae18b67c5e3e6b200b6a342547.

--- koda-client/src/components/HabitatBackground.jsx
+++ koda-client/src/components/HabitatBackground.jsx
@@ -48,7 +48,7 @@ const HabitatBackground = () => {
         <div className="habitat-plain-bg">
           <div
             className="habitat-plain-bg-image"
-            style={{ backgroundImage: `url(${process.env.PUBLIC_URL + "/assets/grass.png"})` }}
+            style={{ backgroundImage: `url(${process.env.PUBLIC_URL + "/lightmode.jpg"})` }}
           />
         </div>
       )}
